3fcec3f728f9d9d8b9cfc77a41a69bf42986287a
zhangyushi <1193822368@qq.com>
2021-03-18 20:41:09 +0800

    果然一个人一个代码习惯

---
+++ src/api/user.js
@@ -0,0 +1,21 @@
+/**
+ * 用户相关请求模块
+ */
+
+import request from '../utils/request'
+
+// 登录
+export const login = data => {
+  return request({
+    method: 'POST',
+    url: '/v1_0/authorizations',
+    data
+  })
+}
+// 发送验证码
+export const sendSms = mobile => {
+  return request({
+    method: 'POST',
+    url: `/v1_0/sms/codes/${mobile}`
+  })
+}
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,21 +3,31 @@
       <!-- 导航栏 -->
       <van-nav-bar class="page-nav-bar" title="登录" />
       <!-- 表单 -->
-      <van-form @submit="onSubmit">
+      <van-form @submit="onSubmit" ref="loginForm">
         <van-field
-          name="用户名"
+          name="mobile"
           placeholder="请输入手机号"
+          v-model="user.mobile"
+          :rules="userFormRules.mobile"
+          type="number"
+          maxlength="11"
         >
           <i slot="left-icon" class="toutiao toutiao-shouji"></i>
         </van-field>
         <van-field
-          type="password"
-          name="验证码"
+          type="number"
+          name="code"
           placeholder="请输入密码"
+          v-model="user.code"
+          :rules="userFormRules.code"
+          maxlength="6"
         >
           <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
           <template #button>
-            <van-button class="send-sms-btn" round size="small" type="default">发送验证码</van-button>
+            <van-count-down v-if="isCountDownShow" :time="1000*10" format="ss s" @finish="isCountDownShow=false"/>
+            <van-button v-else class="send-sms-btn" round size="small" type="default" @click="onSendSms" native-type="button">
+              发送验证码
+            </van-button>
           </template>
         </van-field>
         <div class="login-btn-wrap">
@@ -28,25 +38,79 @@
 </template>
 
 <script type="text/ecmascript-6">
+import { login, sendSms } from '@/api/user'
 export default {
   name: 'LoginIndex',
   data () {
     return {
-      username: '',
-      password: ''
+      user: {
+        mobile: '13911111111',
+        code: '246810'
+      },
+      userFormRules: {
+        mobile: [{ required: true, message: '请填写手机号' }, {
+          pattern: /^1[3|5|7|8]\d{9}$/,
+          message: '手机号格式错误'
+        }],
+        code: [{ required: true, message: '请填写验证码' }, {
+          // 臣妾做不到
+          pattern: /^\d{6}$/,
+          message: '验证码格式错误'
+        }]
+      },
+      isCountDownShow: false
     }
   },
   created () {},
   components: {},
   methods: {
-    onSubmit (values) {
-      console.log('submit', values)
-
+    async onSubmit () {
       // 获取表单数据
-
+      const user = this.user
       // 表单验证
+      this.$toast.loading({
+        message: '登录中...',
+        forbidClick: true,
+        duration: 0
+      })
       // 提交请求
+      try {
+        const { data: res } = await login(user)
+        this.$toast.success('登录成功...')
+        // console.log(res.data, 'vuex传进去没')
+        this.$store.commit('setUser', res.data)
+      } catch (err) {
+        if (err.response.status === 400) {
+          this.$toast.fail('手机号或者验证码错误')
+          console.log('手机号或者验证码错误', '')
+        }
+        console.log('登录失败', err)
+      }
       // 响应结果处理后续操作
+    },
+    async onSendSms () {
+      // 1.校验手机号码
+      try {
+        await this.$refs.loginForm.validate('mobile')
+        console.log('验证通过', '')
+      } catch (err) {
+        return console.log('验证失败', 'err')
+      }
+      // 2.验证通过，显示倒计时
+      this.isCountDownShow = true
+      // 3.请求发生验证码
+      try {
+        await sendSms(this.user.mobile)
+        this.$toast('成功')
+      } catch (err) {
+        this.isCountDownShow = false
+        console.log('获取验证码失败', 'err')
+        if (err.response.status === 429) {
+          this.$toast('发送太频繁，请稍后重试')
+        } else {
+          this.$toast('发送失败')
+        }
+      }
     }
   }
 }
